fe63a71b35f310d46a0cd72e097981b20ddd732c
kimathinjoki <dfkimathinjoki.com>
2024-04-27 17:16:40 -0500

    edit git ignore

--- pollux_back_end/config/routes.rb
+++ pollux_back_end/config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
+  resources :settlements
+  resources :participants
+  resources :activities
+  resources :expenses
   resources :users, only: [:index, :show, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
+  #  user register
   post '/register', to: 'users#register'
+  
+  # login
+  post '/login', to: 'sessions#login'
 
 
 end
