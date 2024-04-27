db62b29fe4fc8cda330ec303f1903d10c314ca84
kimathi Njoki <kimathinjoki@kimathis-MBP.dhcp.umsl.edu>
2024-04-27 14:57:37 -0500

    Add user register

--- pollux_back_end/config/routes.rb
+++ pollux_back_end/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:index, :show, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  post '/register', to: 'users#register'
+
+
 end
